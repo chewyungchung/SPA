68a9a16517a0a0c0452bb5ea03a0cb88f3e837fb
TAN Pheng Heong <phtan90@gmail.com>
2016-09-08 00:41:17 +0800

    Create API definitions in PKB.
    Yung Chung suggested methods that he thought would be useful in the PKB.
    He is in the PQL team, so this commit is the product of inter-team dialogue.
    
    Re: https://cs3201team9.slack.com/archives/general/p1473170357000005
    https://cs3201team9.slack.com/archives/general/p1473166592000004
    https://cs3201team9.slack.com/archives/general/p1473166004000002
    https://cs3201team9.slack.com/archives/general/p1472893791000002
    https://cs3201team9.slack.com/archives/general/p1472822505000002
    https://cs3201team9.slack.com/archives/general/p1472564091000003

--- source/PKB.cpp
+++ source/PKB.cpp
@@ -30,4 +30,111 @@ void PKB::getModifiesUsesTable() {
 	// TODO
 }
 
+int PKB::getParentOf(int stmt) {
+	return 0;
+}
 
+vector<int> PKB::getParentStar(int stmt) {
+	return vector<int>();
+}
+
+vector<int> PKB::getChildrenOf(int stmt) {
+	return vector<int>();
+}
+
+bool PKB::isParentEmpty() {
+	return false;
+}
+
+bool PKB::isParentOf(int parentStmt, int childStmt)
+{
+	return false;
+}
+
+bool PKB::isParentStar(int stmt1, int stmt2) {
+	return false;
+}
+
+int PKB::getFollowedFrom(int stmt) {
+	return 0;
+}
+
+int PKB::getFollower(int stmt) {
+	return 0;
+}
+
+vector<int> PKB::getFollowedFromStar(int stmt) {
+	return vector<int>();
+}
+
+vector<int> PKB::getFollowerStar(int stmt) {
+	return vector<int>();
+}
+
+bool PKB::isFollowEmpty() {
+	return false;
+}
+
+bool PKB::isValidFollows(int followedFrom, int follower) {
+	return false;
+}
+
+bool PKB::isFollowsStar(int stmt1, int stmt2) {
+	return false;
+}
+
+vector<int> PKB::getUsedBy(string name) {
+	return vector<int>();
+}
+
+vector<string> PKB::getUses(int stmt) {
+	return vector<string>();
+}
+
+vector<int, vector<string>> PKB::getAllUses() {
+	return vector<int, vector<string>>();
+}
+
+bool PKB::isUsed(int stmt, string name) {
+	return false;
+}
+
+vector<int> PKB::getModifiedBy(string name) {
+	return vector<int>();
+}
+
+vector<string> PKB::getModifies(int stmt) {
+	return vector<string>();
+}
+
+vector<int, vector<string>> PKB::getAllModifies() {
+	return vector<int, vector<string>>();
+}
+
+bool PKB::isModified(int stmt, string name) {
+	return false;
+}
+
+vector<int> PKB::getAssignList() {
+	return vector<int>();
+}
+
+vector<int> PKB::getWhileList() {
+	return vector<int>();
+}
+
+vector<string> PKB::getVarList() {
+	return vector<string>();
+}
+
+int PKB::getStatementCount() {
+	return 0;
+}
+
+bool PKB::isValidStmt(int stmt) {
+	return false;
+}
+
+vector<int> PKB::getConstantList(){
+	return vector<int>();
+}
--- source/PKB.h
+++ source/PKB.h
@@ -22,4 +22,36 @@ public:
 	void getParentTable();
 	void getModifiesUsesTable();
 
+	int getParentOf(int stmt);
+	vector<int> getParentStar(int stmt);
+	vector<int> getChildrenOf(int stmt);
+	bool isParentEmpty();
+	bool isParentOf(int parentStmt, int childStmt);
+	bool isParentStar(int stmt1, int stmt2);
+
+	int getFollowedFrom(int stmt);
+	int getFollower(int stmt);
+	vector<int> getFollowedFromStar(int stmt);
+	vector<int> getFollowerStar(int stmt);
+	bool isFollowEmpty();
+	bool isValidFollows(int followedFrom, int follower);
+	bool isFollowsStar(int stmt1, int stmt2);
+
+	vector<int> getUsedBy(string name);
+	vector<string> getUses(int stmt);
+	vector<int, vector<string> > getAllUses();
+	bool isUsed(int stmt, string name);
+	vector<int> getModifiedBy(string name);
+	vector<string> getModifies(int stmt);
+	vector<int, vector<string> > getAllModifies();
+	bool isModified(int stmt, string name);
+	
+	vector<int> getAssignList();
+	vector<int> getWhileList();
+
+	vector<string> getVarList();
+
+	int getStatementCount();
+	bool isValidStmt(int stmt);
+	vector<int> getConstantList();
 };
