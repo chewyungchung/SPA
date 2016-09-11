7af318c983f041e071cead967fdd18407226c0b3
timothywee <wee.laiheng.timothy@gmail.com>
2016-09-11 14:30:29 +0800

    Add Parser UnitTest for Uses/Modifies/Constant/StmtType tables

--- UnitTesting/TestParser.cpp
+++ UnitTesting/TestParser.cpp
@@ -1,5 +1,8 @@
 #include "stdafx.h"
+#include <iterator>
+#include <algorithm>
 #include <string>
+#include <vector>
 #include <list>
 #include "CppUnitTest.h"
 #include "Tokenizer.h"
@@ -89,7 +92,6 @@ namespace UnitTesting
 				}
 			*/
 			
-
 			const string NO_STMT = "-1";
 			const string FALSE = "-1";
 
@@ -187,5 +189,306 @@ namespace UnitTesting
 			Assert::AreNotEqual(PKB.getParentOf(10), 6);
 			Assert::AreNotEqual(PKB.getParentOf(12), 6);
 		}
+
+		TEST_METHOD(testModifiesTable)
+		{
+			Parser p("SIMPLE_test_3.txt");
+			p.process();
+			/*
+				procedure XYZ
+				{
+			1		x=1;
+			2		x=z;
+			3		while i 
+					{
+			4			apple = orange;
+			5			banana = pear;
+					}
+			6		while x
+					{
+			7			s = t;
+			8			while y
+						{
+			9				r = 2;
+			10				x = y
+			11				mango = durian;
+						}
+			12			while z
+						{
+			13				papaya = watermelon;
+						}
+					}
+				}
+			*/
+
+			list<string> expectedList;
+			vector<string> expectedVec;
+
+			/* Correct Modifies relationships (assign statements) */
+			// Modifies(1, "x") -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { "x" };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(1), expectedList));
+
+			// Modifies(4, "apple") -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { "apple" };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(4), expectedList));
+
+			// Modifies(11, "mango") -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { "mango" };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(11), expectedList));
+
+			/* Correct Modifies relationships (while statements) */
+			// Modifies(3, v) for v = apple, banana -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { "apple", "banana" };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(3), expectedList));
+
+			// Modifies(6, v) for v = s, r, x, mango, papapya -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { "s", "r", "x", "mango", "papaya" };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(6), expectedList));
+
+			// Modifies(8, v) for v = s, r, x, mango, papapya -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { "r", "x", "mango" };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(8), expectedList));
+		}
+
+		TEST_METHOD(testUsesTable)
+		{
+			Parser p("SIMPLE_test_3.txt");
+			p.process();
+			/*
+				procedure XYZ
+				{
+			1		x=1;
+			2		x=z;
+			3		while i 
+					{
+			4			apple = orange;
+			5			banana = pear;
+					}
+			6		while x
+					{
+			7			s = t;
+			8			while y
+						{
+			9				r = 2;
+			10				x = y
+			11				mango = durian;
+						}
+			12			while z
+						{
+			13				papaya = watermelon;
+						}
+					}
+				}
+			*/
+
+			list<string> expectedList;
+			vector<string> expectedVec;
+
+			/* Correct Uses relationships (assign statements) */
+			// Uses(2, "z") -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { "z" };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getUsedBy(2), expectedList));
+
+			// Uses(4, "orange") -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { "orange" };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getUsedBy(4), expectedList));
+
+			/* Correct Uses relationships (while statements) */
+			// Uses(3, v) for v = i, orange, pear -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { "i", "orange", "pear" };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getUsedBy(3), expectedList));
+
+			// Uses(6, v) for v = i, orange, pear -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { "i", "orange", "pear" };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getUsedBy(3), expectedList));
+
+			// Uses(6, v) for v = x, t, y, durian, z, watermelon -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { "x", "t", "y", "durian", "z", "watermelon" };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getUsedBy(6), expectedList));
+		}
+
+		TEST_METHOD(testConstantTable)
+		{
+			Parser p("SIMPLE_test_3.txt");
+			p.process();
+			/*
+				procedure XYZ
+				{
+			1		x=1;
+			2		x=z;
+			3		while i
+					{
+			4			apple = orange;
+			5			banana = pear;
+					}
+			6		while x
+					{
+			7			s = t;
+			8			while y
+						{
+			9				r = 2;
+			10				x = y
+			11				mango = durian;
+						}
+			12			while z
+						{
+			13				papaya = watermelon;
+						}
+					}
+				}
+			*/
+
+			list<int> expectedList;
+			vector<int> expectedVec;
+			/* Correct Uses relationships (constant not in containers) */
+			// Uses(1, "1") -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { 1 };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getStmtlineByConstant(1), expectedList));
+
+			/* Correct Uses relationships (constant in containers) */
+			// Uses(6, "2"), Uses(8, "2"), Uses(9, "2") -- TRUE
+			clearVector(expectedVec);
+			expectedVec = { 6, 8, 9 };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getStmtlineByConstant(2), expectedList));
+		}
+
+		TEST_METHOD(testStatementTable)
+		{
+			Parser p("SIMPLE_test_3.txt");
+			p.process();
+			/*
+				procedure XYZ
+				{
+			1		x=1;
+			2		x=z;
+			3		while i
+					{
+			4			apple = orange;
+			5			banana = pear;
+					}
+			6		while x
+					{
+			7			s = t;
+			8			while y
+						{
+			9				r = 2;
+			10				x = y
+			11				mango = durian;
+						}
+			12			while z
+						{
+			13				papaya = watermelon;
+						}
+					}
+				}
+			*/
+
+			list<int> expectedList;
+			vector<int> expectedVec;
+			/* Select all assign stmts */
+			// assign a; Select a -- 1, 2, 4, 5, 7, 9, 10, 11, 13
+			clearVector(expectedVec);
+			expectedVec = { 1, 2, 4, 5, 7, 9, 10, 11, 13 };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getAssignList(), expectedList));
+
+			/* Select all while stmts */
+			// while w; Select w -- 3, 6, 8, 12
+			expectedVec = { 3, 6, 8, 12 };
+			vecToListHelper(expectedVec, expectedList);
+			Assert::IsTrue(listCmpHelper(PKB.getWhileList(), expectedList));
+		}
+
+	private:
+
+		bool listCmpHelper(list<string> list1, list<string> list2)
+		{
+			if (list1.size() != list2.size())
+			{
+				return false;
+			}
+			else
+			{
+				list<string>::iterator it = list1.begin();
+				for (; it != list1.end(); ++it)
+				{
+					if (find(list2.begin(), list2.end(), *it) == list2.end())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+		}
+
+		bool listCmpHelper(list<int> list1, list<int> list2)
+		{
+			if (list1.size() != list2.size())
+			{
+				return false;
+			}
+			else
+			{
+				list<int>::iterator it = list1.begin();
+				for (; it != list1.end(); ++it)
+				{
+					if (find(list2.begin(), list2.end(), *it) == list2.end())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+		}
+
+		// Swap with empty vector to free memory
+		void clearVector(vector<string> v)
+		{
+			vector<string>().swap(v);
+		}
+
+		void clearVector(vector<int> v)
+		{
+			vector<int>().swap(v);
+		}
+				
+		void vecToListHelper(vector<string> v, list<string> & s)
+		{
+			s.clear();
+			copy(v.begin(), v.end(), back_inserter(s));
+		}
+
+		void vecToListHelper(vector<int> v, list<int> & s)
+		{
+			s.clear();
+			copy(v.begin(), v.end(), back_inserter(s));
+		}
+
 	};
 }
