c1619b64fe3db5f8fc707dc11b38b07b64971a94
timothywee <wee.laiheng.timothy@gmail.com>
2016-09-11 22:32:34 +0800

    Updated use of PKB API and Tokenizer test to use '+' in RHS assign statements

--- UnitTesting/TestParser.cpp
+++ UnitTesting/TestParser.cpp
@@ -22,42 +22,37 @@ namespace UnitTesting
 		{
 			Tokenizer tk("SIMPLE_test_1.txt");
 			/*
-			1	procedure ABC
+				procedure ABC
 				{
-			3		x=1;
-			4		b=2;
-			5		while i 
+			1.		x=1;
+			2.		b =   2;
+			3.		y = x + i;
+			4.		while i 
 					{
-			6			apple = orange;
+			5.			while   z
+						{
+			6.				good = b2y;
+						}
+			7.			apple = orange;
 					}
 				}
 			*/
 			
 			list<string> expectedList;
-			expectedList.push_back("procedure");
-			expectedList.push_back("ABC");
-			expectedList.push_back("{");
-			expectedList.push_back("x");
-			expectedList.push_back("=");
-			expectedList.push_back("1");
-			expectedList.push_back(";");
-			expectedList.push_back("b");
-			expectedList.push_back("=");
-			expectedList.push_back("2");
-			expectedList.push_back(";");
-			expectedList.push_back("while");
-			expectedList.push_back("i");
-			expectedList.push_back("{");
-			expectedList.push_back("apple");
-			expectedList.push_back("=");
-			expectedList.push_back("orange");
-			expectedList.push_back(";");
-			expectedList.push_back("}");
-			expectedList.push_back("}");
-
+			vector<string> expectedVec;
+			expectedVec = { "procedure", "ABC", "{",
+							"x", "=", "1", ";",
+							"b", "=", "2", ";",
+							"y", "=", "x", "+", "1", ";"
+							"while", "i", "{",
+							"while", "z", "{",
+							"good", "=", "b2y", "}",
+							"apple", "=", "orange", ";", "}", "}"};
+			vecToListHelper(expectedVec, expectedList);
+			
 			list<string>::iterator it = expectedList.begin();
-
-			for (; it != expectedList.end(); ) {
+			
+			for (; it != expectedList.end(); ++it) {
 				Assert::AreEqual(*it, tk.getNextToken());
 			}
 		}
@@ -92,44 +87,44 @@ namespace UnitTesting
 				}
 			*/
 			
-			const string NO_STMT = "-1";
-			const string FALSE = "-1";
+			const int NO_STMT = -1;
+			const int FALSE = -1;
 
 			// Follows(_, x), where x is first statement in each nesting level
 			// Should return NO_STMT
-			Assert::AreEqual(PKB.getFollowedFrom(1), NO_STMT);
-			Assert::AreEqual(PKB.getFollowedFrom(4), NO_STMT);
-			Assert::AreEqual(PKB.getFollowedFrom(7), NO_STMT);
-			Assert::AreEqual(PKB.getFollowedFrom(9), NO_STMT);
-			Assert::AreEqual(PKB.getFollowedFrom(12), NO_STMT);
+			Assert::AreEqual(PKB::getPKB()->getFollowedFrom(1), NO_STMT);
+			Assert::AreEqual(PKB::getPKB()->getFollowedFrom(4), NO_STMT);
+			Assert::AreEqual(PKB::getPKB()->getFollowedFrom(7), NO_STMT);
+			Assert::AreEqual(PKB::getPKB()->getFollowedFrom(9), NO_STMT);
+			Assert::AreEqual(PKB::getPKB()->getFollowedFrom(12), NO_STMT);
 
 			// Follows(x, _) where x is last statement in each nesting level
 			// Should return NO_STMT
-			Assert::AreEqual(PKB.getFollower(6), NO_STMT);
-			Assert::AreEqual(PKB.getFollower(5), NO_STMT);
-			Assert::AreEqual(PKB.getFollower(11), NO_STMT);
-			Assert::AreEqual(PKB.getFollower(10), NO_STMT);
-			Assert::AreEqual(PKB.getFollower(12), NO_STMT);
+			Assert::AreEqual(PKB::getPKB()->getFollower(6), NO_STMT);
+			Assert::AreEqual(PKB::getPKB()->getFollower(5), NO_STMT);
+			Assert::AreEqual(PKB::getPKB()->getFollower(11), NO_STMT);
+			Assert::AreEqual(PKB::getPKB()->getFollower(10), NO_STMT);
+			Assert::AreEqual(PKB::getPKB()->getFollower(12), NO_STMT);
 
 			// Correct Follows relationships
-			Assert::AreEqual(PKB.getFollower(1), 2);
-			Assert::AreEqual(PKB.getFollower(2), 3);
-			Assert::AreEqual(PKB.getFollower(3), 6);
-			Assert::AreEqual(PKB.getFollower(4), 5);
-			Assert::AreEqual(PKB.getFollower(8), 11);
-			Assert::AreEqual(PKB.getFollower(9), 10);
+			Assert::AreEqual(PKB::getPKB()->getFollower(1), 2);
+			Assert::AreEqual(PKB::getPKB()->getFollower(2), 3);
+			Assert::AreEqual(PKB::getPKB()->getFollower(3), 6);
+			Assert::AreEqual(PKB::getPKB()->getFollower(4), 5);
+			Assert::AreEqual(PKB::getPKB()->getFollower(8), 11);
+			Assert::AreEqual(PKB::getPKB()->getFollower(9), 10);
 
 			// Incorrect Follows relationships (same nesting level, not in sequence)
-			Assert::AreNotEqual(PKB.getFollower(1), 3);
-			Assert::AreNotEqual(PKB.getFollower(7), 11);
+			Assert::AreNotEqual(PKB::getPKB()->getFollower(1), 3);
+			Assert::AreNotEqual(PKB::getPKB()->getFollower(7), 11);
 
 			// Incorrect Follows relationships (same nesting level, wrong order)
-			Assert::AreNotEqual(PKB.getFollower(2), 1);
-			Assert::AreNotEqual(PKB.getFollower(11), 8);
+			Assert::AreNotEqual(PKB::getPKB()->getFollower(2), 1);
+			Assert::AreNotEqual(PKB::getPKB()->getFollower(11), 8);
 
 			// Incorrect Follows relationships (different nesting level)
-			Assert::AreNotEqual(PKB.getFollower(3), 4);
-			Assert::AreNotEqual(PKB.getFollower(5), 7);
+			Assert::AreNotEqual(PKB::getPKB()->getFollower(3), 4);
+			Assert::AreNotEqual(PKB::getPKB()->getFollower(5), 7);
 		}
 
 		TEST_METHOD(testParentTable)
@@ -162,32 +157,32 @@ namespace UnitTesting
 				}
 			*/
 
-			const string NO_PARENT = "-1";
-			const string FALSE = "-1";
+			const int NO_PARENT = -1;
+			const int FALSE = -1;
 
 			// Parent(_, x), where x is not in any container
 			// Should return NO_PARENT
-			Assert::AreEqual(PKB.getParentOf(1), NO_PARENT);
-			Assert::AreEqual(PKB.getParentOf(2), NO_PARENT);
-			Assert::AreEqual(PKB.getParentOf(3), NO_PARENT);
-			Assert::AreEqual(PKB.getParentOf(6), NO_PARENT);
+			Assert::AreEqual(PKB::getPKB()->getParentOf(1), NO_PARENT);
+			Assert::AreEqual(PKB::getPKB()->getParentOf(2), NO_PARENT);
+			Assert::AreEqual(PKB::getPKB()->getParentOf(3), NO_PARENT);
+			Assert::AreEqual(PKB::getPKB()->getParentOf(6), NO_PARENT);
 
 			// Correct Parent relationships
-			Assert::AreEqual(PKB.getParentOf(4), 3);
-			Assert::AreEqual(PKB.getParentOf(5), 3);
-			Assert::AreEqual(PKB.getParentOf(7), 6);
-			Assert::AreEqual(PKB.getParentOf(9), 8);
-			Assert::AreEqual(PKB.getParentOf(12), 11);
+			Assert::AreEqual(PKB::getPKB()->getParentOf(4), 3);
+			Assert::AreEqual(PKB::getPKB()->getParentOf(5), 3);
+			Assert::AreEqual(PKB::getPKB()->getParentOf(7), 6);
+			Assert::AreEqual(PKB::getPKB()->getParentOf(9), 8);
+			Assert::AreEqual(PKB::getPKB()->getParentOf(12), 11);
 
 			// Incorrect Parent relationships (container stmt does not match parent)
-			Assert::AreNotEqual(PKB.getParentOf(4), 6);
-			Assert::AreNotEqual(PKB.getParentOf(7), 3);
-			Assert::AreNotEqual(PKB.getParentOf(12), 8);
+			Assert::AreNotEqual(PKB::getPKB()->getParentOf(4), 6);
+			Assert::AreNotEqual(PKB::getPKB()->getParentOf(7), 3);
+			Assert::AreNotEqual(PKB::getPKB()->getParentOf(12), 8);
 
 			// Incorrect Parent relationships 
 			// (not direct parent, i.e. Parent* holds but not Parent)
-			Assert::AreNotEqual(PKB.getParentOf(10), 6);
-			Assert::AreNotEqual(PKB.getParentOf(12), 6);
+			Assert::AreNotEqual(PKB::getPKB()->getParentOf(10), 6);
+			Assert::AreNotEqual(PKB::getPKB()->getParentOf(12), 6);
 		}
 
 		TEST_METHOD(testModifiesTable)
@@ -229,38 +224,38 @@ namespace UnitTesting
 			clearVector(expectedVec);
 			expectedVec = { "x" };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(1), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getModifiedBy(1), expectedList));
 
 			// Modifies(4, "apple") -- TRUE
 			clearVector(expectedVec);
 			expectedVec = { "apple" };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(4), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getModifiedBy(4), expectedList));
 
 			// Modifies(11, "mango") -- TRUE
 			clearVector(expectedVec);
 			expectedVec = { "mango" };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(11), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getModifiedBy(11), expectedList));
 
 			/* Correct Modifies relationships (while statements) */
 			// Modifies(3, v) for v = apple, banana -- TRUE
 			clearVector(expectedVec);
 			expectedVec = { "apple", "banana" };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(3), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getModifiedBy(3), expectedList));
 
 			// Modifies(6, v) for v = s, r, x, mango, papapya -- TRUE
 			clearVector(expectedVec);
 			expectedVec = { "s", "r", "x", "mango", "papaya" };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(6), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getModifiedBy(6), expectedList));
 
 			// Modifies(8, v) for v = s, r, x, mango, papapya -- TRUE
 			clearVector(expectedVec);
 			expectedVec = { "r", "x", "mango" };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getModifiedBy(8), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getModifiedBy(8), expectedList));
 		}
 
 		TEST_METHOD(testUsesTable)
@@ -302,32 +297,32 @@ namespace UnitTesting
 			clearVector(expectedVec);
 			expectedVec = { "z" };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getUsedBy(2), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getUsedBy(2), expectedList));
 
 			// Uses(4, "orange") -- TRUE
 			clearVector(expectedVec);
 			expectedVec = { "orange" };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getUsedBy(4), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getUsedBy(4), expectedList));
 
 			/* Correct Uses relationships (while statements) */
 			// Uses(3, v) for v = i, orange, pear -- TRUE
 			clearVector(expectedVec);
 			expectedVec = { "i", "orange", "pear" };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getUsedBy(3), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getUsedBy(3), expectedList));
 
 			// Uses(6, v) for v = i, orange, pear -- TRUE
 			clearVector(expectedVec);
 			expectedVec = { "i", "orange", "pear" };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getUsedBy(3), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getUsedBy(3), expectedList));
 
 			// Uses(6, v) for v = x, t, y, durian, z, watermelon -- TRUE
 			clearVector(expectedVec);
 			expectedVec = { "x", "t", "y", "durian", "z", "watermelon" };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getUsedBy(6), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getUsedBy(6), expectedList));
 		}
 
 		TEST_METHOD(testConstantTable)
@@ -368,14 +363,14 @@ namespace UnitTesting
 			clearVector(expectedVec);
 			expectedVec = { 1 };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getStmtlineByConstant(1), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getStmtlineByConstant(1), expectedList));
 
 			/* Correct Uses relationships (constant in containers) */
 			// Uses(6, "2"), Uses(8, "2"), Uses(9, "2") -- TRUE
 			clearVector(expectedVec);
 			expectedVec = { 6, 8, 9 };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getStmtlineByConstant(2), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getStmtlineByConstant(2), expectedList));
 		}
 
 		TEST_METHOD(testStatementTable)
@@ -416,13 +411,13 @@ namespace UnitTesting
 			clearVector(expectedVec);
 			expectedVec = { 1, 2, 4, 5, 7, 9, 10, 11, 13 };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getAssignList(), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getAssignList(), expectedList));
 
 			/* Select all while stmts */
 			// while w; Select w -- 3, 6, 8, 12
 			expectedVec = { 3, 6, 8, 12 };
 			vecToListHelper(expectedVec, expectedList);
-			Assert::IsTrue(listCmpHelper(PKB.getWhileList(), expectedList));
+			Assert::IsTrue(listCmpHelper(PKB::getPKB()->getWhileList(), expectedList));
 		}
 
 	private:
