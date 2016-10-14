a5e8d8800a0e3f75ed723a0ceaab3558de8f267b
timothywee <wee.laiheng.timothy@gmail.com>
2016-10-14 13:17:50 +0800

    Added ProcTable

--- SPA/source/ProcTable.h
+++ SPA/source/ProcTable.h
@@ -3,6 +3,7 @@
 #include <list>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
@@ -15,7 +16,29 @@ public:
 	void addProc(string procName);
 	void addProcMod(string procName, string var);
 	void addProcUses(string procName, string var);
+	void addProcCalledInStmt(string procName, int stmtLine);
 
 	string getProcName(int procIndex);
-	int getProcIndex(string procName)
+	int getProcIndex(string procName);
+
+	bool isModifiedByProc(string procName, string varName);
+	bool isUsedByProc(string procName, string varName);
+	list<string> getModifiedByProc(string procName);
+	list<string> getusedByProc(string procName);
+	list<string> getProcedureModifying(string varName);
+	list<string> getProcedureUsing(string varName);
+
+	list<string> getCalledProcNamesList();
+	list<string> getProcedureList();
+
+private:
+	int PROCDATA_COLSIZE = 4;
+	int PROCNAME_COL = 0;
+	int MODVAR_COL = 1;
+	int USEVAR_COL = 2;
+	int CALLED_IN_STMT_COL = 3;
+
+	unordered_map<string, int> procNameIndexTable; //key: procName; value: internal index (0, 1, 2, ..)
+	vector<vector<list<string>>> procDataTable; // for every procIndex: col_1: procName; col_2: modified var, col_3: used var, col_4: called in stmtLine
+	int maxProcIndex;
 };
