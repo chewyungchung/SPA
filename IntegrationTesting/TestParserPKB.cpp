424661706b907775425043aaf83969601b14ee91
timothywee <wee.laiheng.timothy@gmail.com>
2016-10-15 14:22:25 +0800

    Refactored Parser-PKB component tests into IntegrationTesting, added iteration2 test for tokenizer

--- IntegrationTesting/TestParserPKB.cpp
+++ IntegrationTesting/TestParserPKB.cpp
@@ -1,186 +1,37 @@
 /*
-To unit test the PKB, we manually populate it directly by 'add' methods and test the methods
+Integration testing for Parser-PKB component interaction
+Parser is given source file and populates PKB
+The test check if PKB is populated correctly
 */
 #include "stdafx.h"
-
-#include "stdafx.h"
 #include "CppUnitTest.h"
+
 #include <iterator>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include "CppUnitTest.h"
+#include "Tokenizer.h"
+#include "Parser.h"
+#include "DesignExtractor.h"
 #include "PKB.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
-namespace UnitTesting
-{
-	TEST_CLASS(TestPKB)
+namespace IntegrationTesting
+{		
+	TEST_CLASS(TestParserPKB)
 	{
-	public:
-
-		TEST_METHOD_INITIALIZE(buildPKB)
-		{			
-		}
-
-		TEST_METHOD_CLEANUP(destroyPKB)
-		{
-		}
-
-		TEST_METHOD(TestModUsesbyStmtFunction_IT1)
+	public:		
+		TEST_METHOD(TestParserFollowsTable_IT1)
 		{
 			PKB _pkb;
-			/* addModUsesbyStmt */
-			_pkb.addModifies(1, "a");
-			_pkb.addModifies(2, "b");
-			_pkb.addModifies(2, "c");
-			_pkb.addModifies(2, "z");
-			_pkb.addModifies(3, "a");
-			_pkb.addModifies(4, "xyz");
+			// Test if FollowsTable is empty before being populated
 
-			_pkb.addUses(1, "a");
-			_pkb.addUses(1, "i");
-			_pkb.addUses(2, "d");
-			_pkb.addUses(2, "i");
-			_pkb.addUses(5, "e");
-			_pkb.addUses(5, "a");
-
-			list<string> actual, expected;
-
-			actual = _pkb.getModifiedBy(1);
-			expected = { "a" };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getModifiedBy(2);
-			expected = { "b", "c", "z" };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getModifiedBy(4);
-			expected = { "xyz" };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getUsedBy(1);
-			expected = { "a", "i" };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getUsedBy(2);
-			expected = { "d", "i" };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getUsedBy(5);
-			expected = { "e", "a" };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getUsedBy(10);
-			expected = {};
-			Assert::IsTrue(actual == expected);
-
-			Assert::IsTrue(_pkb.isModified(1, "a"));
-			Assert::IsTrue(_pkb.isModified(2, "b"));
-			Assert::IsTrue(_pkb.isModified(2, "c"));
-			Assert::IsTrue(_pkb.isModified(2, "z"));
-			Assert::IsTrue(_pkb.isModified(4, "xyz"));
-			/* -1 is not a existent statement statement no(key)*/
-			Assert::IsFalse(_pkb.isModified(-1, "a"));
-			/* "xyz" exist, but stmt 2 is not its corresponding key*/
-			Assert::IsFalse(_pkb.isModified(2, "xyz"));
-			/* "_xyz_" does not exist in the modTable*/
-			Assert::IsFalse(_pkb.isModified(4, "_xyz_"));
-
-			Assert::IsTrue(_pkb.isUsed(1, "a"));
-			Assert::IsTrue(_pkb.isUsed(1, "i"));
-			Assert::IsTrue(_pkb.isUsed(2, "d"));
-			Assert::IsTrue(_pkb.isUsed(5, "e"));
-			Assert::IsFalse(_pkb.isUsed(-1, "a"));
-			Assert::IsFalse(_pkb.isUsed(5, "i"));
-		}
-
-		TEST_METHOD(TestModUsesbyVarFunction_IT1)
-		{
-			PKB _pkb;
-			/* addModUsesbyVar */
-			_pkb.addModifies("a", 1);
-			_pkb.addModifies("a", 3);
-			_pkb.addModifies("b", 2);
-			_pkb.addModifies("c", 2);
-			_pkb.addModifies("z", 2);
-			_pkb.addModifies("xyz", 4);
-
-			_pkb.addUses("a", 1);
-			_pkb.addUses("a", 5);
-			_pkb.addUses("i", 1);
-			_pkb.addUses("i", 2);
-			_pkb.addUses("d", 2);
-			_pkb.addUses("e", 5);
-
-			list<int> actual, expected;
-			list<string> actualVarList, expectedVarList;
-
-			actual = _pkb.getModifiedBy("a");
-			expected = { 1,3 };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getModifiedBy("b");
-			expected = { 2 };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getModifiedBy("c");
-			expected = { 2 };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getModifiedBy("z");
-			expected = { 2 };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getModifiedBy("xyz");
-			expected = { 4 };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getUsedBy("a");
-			expected = { 1, 5 };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getUsedBy("i");
-			expected = { 1, 2 };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getUsedBy("d");
-			expected = { 2 };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getUsedBy("e");
-			expected = { 5 };
-			Assert::IsTrue(actual == expected);
-
-			actual = _pkb.getUsedBy("invalid");
-			expected = {};
-			Assert::IsTrue(actual == expected);
-
-			/* all the variables from the mod and uses table should be valid */
-			Assert::IsTrue(_pkb.isValidVar("a"));
-			Assert::IsTrue(_pkb.isValidVar("b"));
-			Assert::IsTrue(_pkb.isValidVar("c"));
-			Assert::IsTrue(_pkb.isValidVar("z"));
-			Assert::IsTrue(_pkb.isValidVar("xyz"));
-			Assert::IsTrue(_pkb.isValidVar("i"));
-			Assert::IsTrue(_pkb.isValidVar("d"));
-			Assert::IsTrue(_pkb.isValidVar("e"));
-
-			actualVarList = _pkb.getAllModVar();
-			expectedVarList = { "a", "b", "c", "z", "xyz" };
-			Assert::IsTrue(actual == expected);
-
-			/* clear the existing modVarList and check if the assertion is
-			correct for the case where the list is empty */
-			actualVarList = _pkb.getAllUsedVar();
-			expectedVarList = { "a", "i", "d", "e" };
-			Assert::IsTrue(actual == expected);
-
-			actualVarList = _pkb.getVarList();
-			expectedVarList = { "a", "b", "c", "z", "xyz", "i", "d", "e" };
-			Assert::IsTrue(actual == expected);
-		}
-
-		TEST_METHOD(TestFollowsTable_IT1)
-		{
-			PKB _pkb;
+			Parser p("SIMPLE_test_2.txt");
+			_pkb = p.process();
 
 			/*
 				procedure ABC
@@ -208,19 +59,6 @@ namespace UnitTesting
 				}
 			*/
 
-			_pkb.addFollows(1, 1);
-			_pkb.addFollows(2, 1);
-			_pkb.addFollows(3, 1);
-			_pkb.addFollows(4, 2);
-			_pkb.addFollows(5, 2);
-			_pkb.addFollows(6, 1);
-			_pkb.addFollows(7, 3);
-			_pkb.addFollows(8, 3);
-			_pkb.addFollows(9, 4);
-			_pkb.addFollows(10, 4);
-			_pkb.addFollows(11, 3);
-			_pkb.addFollows(12, 5);
-			
 			const int NO_STMT = -1;
 			list<int> expectedList;
 			vector<int> expectedVec;
@@ -315,9 +153,11 @@ namespace UnitTesting
 			Assert::IsTrue(listCmpHelper(_pkb.getFollowerStar(7), expectedList));
 		}
 
-		TEST_METHOD(TestParentTable)
+		TEST_METHOD(TestParserParentTable_IT1)
 		{
 			PKB _pkb;
+			Parser p("SIMPLE_test_2.txt");
+			_pkb = p.process();
 
 			/*
 				procedure ABC
@@ -345,8 +185,6 @@ namespace UnitTesting
 				}
 			*/
 
-			_pkb.addParent()
-
 			const int NO_PARENT = -1;
 			const int FALSE = -1;
 
@@ -371,22 +209,21 @@ namespace UnitTesting
 
 			// Incorrect Parent relationships 
 			// (not direct parent, i.e. Parent* holds but not Parent)
-
 			Assert::AreNotEqual(_pkb.getParentOf(10), 6);
 			Assert::AreNotEqual(_pkb.getParentOf(12), 6);
 		}
 
-		TEST_METHOD(TestModifiesTable)
+		TEST_METHOD(TestParserModifiesTable_IT1)
 		{
 			PKB _pkb;
 			Parser p("SIMPLE_test_3.txt");
 			_pkb = p.process();
 
 			/*
-				procedure ABC
+				procedure XYZ
 				{
-			1		x = 1;
-			2		b = 2;
+			1		x=1;
+			2		x=z;
 			3		while i
 					{
 			4			apple = orange;
@@ -398,11 +235,12 @@ namespace UnitTesting
 			8			while y
 						{
 			9				r = 2;
-			10				mango = durian;
+			10				x = y;
+			11				mango = durian;
 						}
-			11			while z
+			12			while z
 						{
-			12				papaya = watermelon;
+			13				papaya = watermelon;
 						}
 					}
 				}
@@ -450,17 +288,17 @@ namespace UnitTesting
 			Assert::IsTrue(listCmpHelper(_pkb.getModifiedBy(8), expectedList));
 		}
 
-		TEST_METHOD(TestUsesTable)
+		TEST_METHOD(TestParserUsesTable_IT1)
 		{
 			PKB _pkb;
 			Parser p("SIMPLE_test_3.txt");
 			_pkb = p.process();
 
 			/*
-				procedure ABC
+				procedure XYZ
 				{
-			1		x = 1;
-			2		b = 2;
+			1		x=1;
+			2		x=z;
 			3		while i
 					{
 			4			apple = orange;
@@ -472,11 +310,12 @@ namespace UnitTesting
 			8			while y
 						{
 			9				r = 2;
-			10				mango = durian;
+			10				x = y;
+			11				mango = durian;
 						}
-			11			while z
+			12			while z
 						{
-			12				papaya = watermelon;
+			13				papaya = watermelon;
 						}
 					}
 				}
@@ -518,36 +357,37 @@ namespace UnitTesting
 			Assert::IsTrue(listCmpHelper(_pkb.getUsedBy(6), expectedList));
 		}
 
-		TEST_METHOD(TestConstantTable1)
+		TEST_METHOD(TestConstantTable_IT1)
 		{
 			PKB _pkb;
 			Parser p("SIMPLE_test_3.txt");
 			_pkb = p.process();
+
 			/*
-			procedure XYZ
-			{
+				procedure XYZ
+				{
 			1		x=1;
 			2		x=z;
 			3		while i
-			{
+					{
 			4			apple = orange;
 			5			banana = pear;
-			}
+					}
 			6		while x
-			{
+					{
 			7			s = t;
 			8			while y
-			{
+						{
 			9				r = 2;
-			10				x = y
+			10				x = y;
 			11				mango = durian;
-			}
+						}
 			12			while z
-			{
+						{
 			13				papaya = watermelon;
-			}
-			}
-			}
+						}
+					}
+				}
 			*/
 
 			list<int> expectedList;
@@ -567,36 +407,37 @@ namespace UnitTesting
 			Assert::IsTrue(listCmpHelper(_pkb.getStmtlineByConstant(2), expectedList));
 		}
 
-		TEST_METHOD(TestStatementTable)
+		TEST_METHOD(TestParserStatementTable_IT1)
 		{
 			PKB _pkb;
 			Parser p("SIMPLE_test_3.txt");
 			_pkb = p.process();
+
 			/*
-			procedure XYZ
-			{
+				procedure XYZ
+				{
 			1		x=1;
 			2		x=z;
 			3		while i
-			{
+					{
 			4			apple = orange;
 			5			banana = pear;
-			}
+					}
 			6		while x
-			{
+					{
 			7			s = t;
 			8			while y
-			{
+						{
 			9				r = 2;
-			10				x = y
+			10				x = y;
 			11				mango = durian;
-			}
+						}
 			12			while z
-			{
+						{
 			13				papaya = watermelon;
-			}
-			}
-			}
+						}
+					}
+				}
 			*/
 
 			list<int> expectedList;
@@ -615,55 +456,7 @@ namespace UnitTesting
 			Assert::IsTrue(listCmpHelper(_pkb.getWhileList(), expectedList));
 		}
 
-		TEST_METHOD(testStatementFunction)
-		{
-			PKB _pkb;
-
-			_pkb.addStatement(1, "assign");
-			_pkb.addStatement(2, "assign");
-			//_pkb.addStatement(1, "while"); //invalid case
-			_pkb.addStatement(3, "assign");
-			_pkb.addStatement(5, "while");
-			_pkb.addStatement(6, "assign");
-			_pkb.addStatement(7, "invalidtype"); //invalid
-			_pkb.addStatement(8, "assign");
-			_pkb.addStatement(9, "while");
-			_pkb.addStatement(10, "assign");
-
-			list<int> actual, expected1, expected2;
-			int actualTotal, expectedTotal;
-
-			actual = _pkb.getAssignList();
-			expected1 = { 1, 2, 3, 6, 8, 10 };
-			expected2 = { 1, 2, 3, 6, 7, 8, 10 };
-			Assert::IsTrue(actual == expected1);
-			/* stmt 7 should not be in the list; stmtType is invalid */
-			Assert::IsFalse(actual == expected2);
-			
-			actual = _pkb.getWhileList();
-			expected1 = {5, 9};
-			expected2 = {5, 7, 9};
-			Assert::IsTrue(actual == expected1);
-			/* stmt 7 should not be in the list; stmtType is invalid */
-			Assert::IsFalse(actual == expected2);
-
-			actual = _pkb.getStmtList();
-			expected1 = { 1, 2, 3, 5, 6, 8, 9, 10 };
-			Assert::IsTrue(actual == expected1);
-			
-			actualTotal = _pkb.getStatementCount();
-			expectedTotal = 8;
-			Assert::IsTrue(actualTotal == expectedTotal);
-		///* check isValidStmt */
-		Assert::IsTrue(_pkb.isValidStmt(1));
-		Assert::IsTrue(_pkb.isValidStmt(2));
-		Assert::IsTrue(_pkb.isValidStmt(3));
-		Assert::IsFalse(_pkb.isValidStmt(4));
-		Assert::IsTrue(_pkb.isValidStmt(5));
-		Assert::IsFalse(_pkb.isValidStmt(11));
-		}
-
-	private:
+	public:
 
 		bool listCmpHelper(list<string> list1, list<string> list2)
 		{
@@ -715,7 +508,7 @@ namespace UnitTesting
 		{
 			vector<int>().swap(v);
 		}
-				
+
 		void vecToListHelper(vector<string> v, list<string> & s)
 		{
 			s.clear();
@@ -727,6 +520,5 @@ namespace UnitTesting
 			s.clear();
 			copy(v.begin(), v.end(), back_inserter(s));
 		}
-
 	};
 }
