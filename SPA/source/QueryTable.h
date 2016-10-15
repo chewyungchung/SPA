49b0809c0a0f65659f4f13eda9d698e83cbe6e95
chewyungchung <a0133662@u.nus.edu>
2016-10-15 18:27:43 +0800

    Added query optimization (grouping) of clauses for iteration 2

--- SPA/source/QueryTable.h
+++ SPA/source/QueryTable.h
@@ -14,24 +14,21 @@ using namespace std;
 
 class QueryTable {
 public:
+	struct ClauseComparator {
+		bool operator() (Clause lhs, Clause rhs) {
+			return lhs.GetPriority() > rhs.GetPriority();
+		}
+	};
 
+	// Constructors
 	QueryTable();
-	QueryTable(int is_null);
+	QueryTable(bool is_null);
 	QueryTable(Clause select_clause, vector<Clause> with_clauses, vector<Clause> such_that_clauses, vector<Clause> pattern_clauses);
 
+	// Optimization - Public
 	void Optimize();
-	void InitialiseWeightTable();
-	void ReplaceWithClauses();
-	void GroupClauses();
-	void SortClausesInGroup();
-	void SortGroups();
-	void PopulateNonSynClauses();
-	void PopulateSynClauses();
-	void GroupSynClauses();
 
-	int GetNumOfSynInClause(vector<string> arg1_type, vector<string> arg2_type);
-
-	// Get
+	// Getters
 	Clause GetSelectClause();
 	vector<Clause> GetSuchThatClauses();
 	vector<Clause> GetWithClauses();
@@ -39,13 +36,11 @@ public:
 	vector<Clause> GetNoSynGroup();
 	vector<Clause> GetNonConnectedGroup();
 	vector<Clause> GetConnectedGroup();
-
-	int IsNullQuery();
-	bool IsSynonym(string arg);
-	bool IsSynFound(Clause clause, string syn, string relation);
+	vector<vector<Clause>> GetConnectedGroups();
 	string GetSynType(string syn_type);
+	bool IsNullQuery();
 
-	// Set
+	// Setters
 	void SetSelectClause(Clause select_clause);
 	void SetSynEntityMap(unordered_map<string, string> synToEntityMap);
 	void AddWithClause(Clause with_clause);
@@ -53,20 +48,45 @@ public:
 	void AddPatternClause(Clause pattern_clause);
 
 private:
-	unordered_map<string, string> syn_entity_map_; 
+	// Optimization - Private
+	void CategorizeClauses();
+	void BuildAllClause();
+	void PopulateWithConstantClauses();
+	void PopulateNoSynClauses();
+	void PopulateOneSynClauses();
+	void PopulateTwoSynClauses();
+	void PopulateNonConnectedGroup();
+	void EvaluateWithConstantClauses();
+	void GroupClauses();
+	int GetNumOfSynInClause(Clause clause);
+	void ReplaceWithClauses(); // Not used yet
+
+	// Getters - Private
+	string GetSecondSynonym(Clause clause, string select_syn);
+	bool IsConstantWithClause(Clause with_clause);
+	bool HasSelectedSynonym(Clause clause);
+	bool IsSynonym(string arg);
+	bool IsSynFound(Clause clause, string synonym);
+	bool IsSynSelected(Clause select_clause);
+
+	unordered_map<string, string> syn_entity_map_;
+	bool is_null_;
+
+	// Original Clauses
 	Clause select_clause_;
 	vector<Clause> such_that_clauses_;
-	vector<Clause> with_clauses_;
 	vector<Clause> pattern_clauses_;
+	vector<Clause> with_clauses_;
 
+	// Groups
 	vector<Clause> no_syn_group_;
-	vector<Clause> with_syn_clauses_;
-
-	vector<Clause> connected_group_;
-	//vector<vector<Clause>> connected_groups_;
 	vector<Clause> non_connected_group_;
+	vector<Clause> connected_group_;
+	vector<vector<Clause>> connected_groups_;
 
-	//WeightTable _weightTable;
-
-	int is_null_;
+	// Main Structures
+	vector<Clause> all_clauses_;
+	vector<Clause> one_syn_clauses_;
+	vector<Clause> two_syn_clauses_;
+	vector<Clause> with_constant_clauses_;
 };
