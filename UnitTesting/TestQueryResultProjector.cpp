89b502ca6818e3d5b2e467c165a758d735f62970
TAN Pheng Heong <phtan90@gmail.com>
2016-10-16 00:31:35 +0800

    Create unit test based on iteration 2 API.

--- UnitTesting/TestQueryResultProjector.cpp
+++ UnitTesting/TestQueryResultProjector.cpp
@@ -1,71 +1,49 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "Parser.h"
-#include "PKB.h"
-#include "QueryValidator.h"
-#include "QueryEvaluator.h"
+
 #include "QueryResultProjector.h"
-#include "QueryTokenizer.h"
-#include "QueryToken.h"
-#include "QueryTable.h"
-#include "QueryException.h"
-#include "RelationTable.h"
-#include "Relation.h"
 #include "Clause.h"
+#include "ResultTable.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
 namespace UnitTesting {
 	TEST_CLASS(TestQueryResultProjector) {
-		TEST_METHOD(testBasicQueryOutput) {
-			Parser _parser;
-			PKB _pkb;
-			QueryEvaluator _qe;
-			QueryValidator _qv;
+		TEST_METHOD(testConstructors) {
+			
 			QueryResultProjector _qrp;
-			QueryTable _qt;
-			QueryTable _qtResults;
+			
 			list<string> finalResults;
-			string query;
-			vector<string> selectResults;
-			vector<string> suchThatResults1;
-			vector<string> suchThatResults2;
-			vector<string> patternSynResults;
-			vector<string> patternArg1Results;
-
-			query = "assign a; Select a pattern a(_, _\"1\"_)"; // y
-			_parser = Parser("C:\\Users\\Einlanz\\Documents\\GitSPA\\AutoTester\\Release\\PATTERN_SOURCE_01.txt");
-			_qv = QueryValidator(query);
-			_qt = _qv.parse();
-
-			Assert::AreEqual(_qt.isNullQuery(), -1,L"NULL query");
 			
-			_pkb = _parser.process();
-			_qe = QueryEvaluator(_qt, _pkb);
-			_qtResults = _qe.evaluate();
-			
-			//Assert::AreEqual(_qtResults.isNullQuery(), -1, L"NULL results");
-			//Assert::AreEqual(_qtResults.isSelectResultEmpty(), -1 , L"Select not suppose to empty");
-			//Assert::AreEqual(_qtResults.isSuchThatResultEmpty(), 1, L"Such suppose to empty");
-			//Assert::AreEqual(_qtResults.isPatternResultEmpty(), -1, L"Pattern not suppose to empty");
-			//
-			//selectResults = _qtResults.getSelectResult().getArg1ResultList();
-			//suchThatResults1 = _qtResults.getSuchThatResult().getArg1ResultList();
-			//suchThatResults2 = _qtResults.getSuchThatResult().getArg2ResultList();
-			//patternSynResults = _qtResults.getPatternResult().getPatternResultList();
-			//patternArg1Results = _qtResults.getPatternResult().getArg1ResultList();
+			ResultTable rt;
+			rt = ResultTable("a1");
+			ResultTable r2d2;
+			r2d2 = ResultTable("r2d2");
+			vector<ResultTable> vrt;
+			vrt.push_back(rt);
+			vrt.push_back(r2d2);
+			ResultTable rt2 = ResultTable("a1");
+			ResultTable c3po = ResultTable("c3po");
+			vector<ResultTable> vrt2;
+			vrt2.push_back(rt2);
+			vrt2.push_back(c3po);
+			vector<vector<ResultTable>> vvrt;
+			vvrt.push_back(vrt);
+			vvrt.push_back(vrt2);
 
-			//Assert::AreEqual(selectResults.size(), (size_t)17, L"SelectResult");
-			//Assert::AreEqual(suchThatResults1.size(), (size_t)0,L"SuchThat ONE"); 
-			//Assert::AreEqual(suchThatResults2.size(), (size_t)0, L"SuchThat TWO");
-			//Assert::AreEqual(patternSynResults.size(), (size_t)1, L"Pattern SYN"); // 4
-			//Assert::AreEqual(patternArg1Results.size(), (size_t)0, L"Pattern ONE"); // y
+			Clause c;
+			vector<string> args;
+			args.push_back("a1");
+			vector<string> arg_types;
+			arg_types.push_back("nonsense");
+			c = Clause("NONSENSE", args, arg_types);
+			_qrp = QueryResultProjector(vvrt, c);
 
-			_qrp = QueryResultProjector(_qtResults,_pkb);
-			finalResults = _qrp.getResults();
-			finalResults.unique();
-			Assert::AreEqual(finalResults.size(), (size_t)1, L"FINAL RESULTS SIZE");
+			finalResults = _qrp.GetResults();
+			size_t NUM_OF_COLUMNS_AFTER_INNER_JOIN = 3;
+
+			Assert::AreEqual(NUM_OF_COLUMNS_AFTER_INNER_JOIN, finalResults.size(), L"FINAL RESULTS SIZE");
 
 
 
