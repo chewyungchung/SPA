ed5e61536cf70aa33c9e169f52ad6db2a06269b1
chewyungchung <a0133662@u.nus.edu>
2016-10-15 20:01:41 +0800

    Reboot with new stubs for iteration 2

--- SPA/source/QueryResultProjector.h
+++ SPA/source/QueryResultProjector.h
@@ -1,44 +1,24 @@
-//#pragma once
-//#include <unordered_map>
-//#include <vector>
-//#include <string>
-//#include <algorithm>
-//
-//#include "QueryTable.h"
-//#include "PKB.h"
-//
-//using namespace std;
-//
-//class QueryResultProjector {
-//public:
-//	QueryResultProjector();
-//	~QueryResultProjector();
-//	QueryResultProjector(QueryTable);
-//	QueryResultProjector(QueryTable, PKB);
-//	list<string> getResults();
-//
-//	// Check between all clauses
-//	int isResultShareCommonSyn(string selectSyn, QueryResult suchThatResult, QueryResult patternResult);
-//	vector<string> getClauseSynonym(string clause, QueryResult);
-//	// Does not involve select
-//	list<string> mergeResult(QueryResult suchThatResult, QueryResult patternResult);
-//	// Involve select
-//	list<string> mergeResult(string selectSyn, QueryResult suchThatResult, QueryResult patternResult);
-//	list<string> getListIntersection(list<string> result1, list<string> result2);
-//	list<string> getListResult(vector<string> vectorResults);
-//	list<string> getSynResult(string syn, string clause, QueryResult clauseResult);
-//	list<string> getCommonSynonym(QueryResult suchThatResult, QueryResult patternResult);
-//	list<string> checkAgain(list<string> interserctionResults, string selectSyn, string commonSyn);
-//
-//	unordered_map<string, list<string>> getCommonSynonymResult(QueryResult suchThatResult, QueryResult patternResult);
-//	unordered_map<string, list<string>> getCommonSynonymResult(list<string> commonSyn, QueryResult suchThatResult, QueryResult patternResult);
-//
-//private:
-//	QueryTable _qt;
-//	PKB _pkb;
-//	int _selectExist = -1;
-//	int _suchThatExist = -1;
-//	int _patternExist = -1;
-//	int _suchThatNull = 1;
-//	int _patternNull = 1;
-//};
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#include "QueryTable.h"
+#include "ResultTable.h"
+
+using namespace std;
+
+class QueryResultProjector {
+public:
+	QueryResultProjector();
+	QueryResultProjector(vector<vector<ResultTable>> intermediate_results, Clause select_clause);
+
+	list<string> GetResults();
+	void InnerJoin(ResultTable instance_one, ResultTable instance_two);
+
+private:
+	vector<vector<ResultTable>> intermediate_results_;
+	Clause select_clause_;
+};
