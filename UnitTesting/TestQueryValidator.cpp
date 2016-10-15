00413b6cc1bb52ee1ea53dffd5848cc3abee3e14
chewyungchung <a0133662@u.nus.edu>
2016-10-15 18:29:51 +0800

    Updated with brief test for optimization in QueryTable

--- UnitTesting/TestQueryValidator.cpp
+++ UnitTesting/TestQueryValidator.cpp
@@ -24,18 +24,25 @@ namespace UnitTesting {
 			string query_clause = "such that Follows(s1,s2) and Follows*(s1,s2) and Parent(w1,n2) and Parent*(if1,n2) such that Call(proc1,proc2) and Call*(proc1,proc2) and Next(n1,a2) and Next*(n2, w2) and Modifies(n1,\"x\") and Modifies(proc1,v1) and Modifies(\"main\",\"var2\") and Uses(if1,\"y\") and Uses(proc2, v2) and Uses(\"main\", \"var1\")";
 			string query_string = declaration + query_select + query_clause;
 			QueryValidator query_parser(query_string);
-			QueryTable query_tree = query_parser.parse();
+			QueryTable query_tree = query_parser.Parse();
 
 			//query_clause = "pattern if1(_,_,_) and w1(_,_) pattern if2(v1,_,_) and w2(v2,_) and a1(_,_) and a2(v1,_) and a1(_,_\"x+y\"_) and a2(v1,\"x+y\")";
 			query_clause = "pattern a1(_, \"x + y * 5 + (3+2) * 9\")";
 			query_string = declaration + query_select + query_clause;
 			query_parser = QueryValidator(query_string);
-			query_tree = query_parser.parse();
+			query_tree = query_parser.Parse();
 
 			query_clause = "with s1.stmt# = s2.stmt# and n1 = 5 and 5 = n2 with n1 = c1.value and proc1.procName = \"main\" and \"var1\" = v1.varName and 5 = 4 and \"test\" = \"test1\" and a1.stmt# = 2 and if1.stmt# = w1.stmt# and call1.procName = \"test\" and 9 = call2.stmt#";
 			query_string = declaration + query_select + query_clause;
 			query_parser = QueryValidator(query_string);
-			query_tree = query_parser.parse();
+			query_tree = query_parser.Parse();
+
+			query_clause = "such that Follows(a2,5) and Follows(1,2) and Uses(5,\"y\") and Parent(if1,a1) and Parent(if2,a2) and Next(a2,n2) and Next(n2,s1) and Modifies(s1,v1) and Uses(proc1,v1) and Call(proc1,proc2) and Modifies(s1,v1) and Uses(proc1,v1) with a2.stmt#=1 and 1=0";
+			query_string = declaration + query_select + query_clause;
+			query_parser = QueryValidator(query_string);
+			query_tree = query_parser.Parse();
+
+			query_tree.Optimize();
 
 			cout << "All is well" << endl;
 		}
