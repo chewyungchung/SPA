8d171576834e344bb65f8dd5b2f2805bf28319ba
Kwok Jun Kiat <knightlordis@gmail.com>
2016-10-15 21:47:35 +0800

    Bug fix for cfg

--- SPA/source/CFG.cpp
+++ SPA/source/CFG.cpp
@@ -71,25 +71,20 @@ void CFG::closeWhileCFG()
 void CFG::buildCFGMatrix()
 {
 	int size = nodeTable.size + 1;
-	int** graph = new int*[size];
+	int** matrix = new int*[size];
 	for (int i = 0; i < size; ++i) {
-		graph[i] = new int[size];
+		matrix[i] = new int[size];
 	}
 
 	for (auto map : nodeTable) {
 		for (Node n : map.second.getNextList()) {
-			graph[map.first][n.getStmtnum] = 1;
+			matrix[map.first][n.getStmtnum] = 1;
 		}
 	}
 
-	matrix = new int*[size];
-	int i, j, k;
-	for (i = 0; i < size; i++)
-		for (j = 0; j < size; j++)
-			matrix[i][j] = graph[i][j];
-	for (k = 0; k < size; k++) {
-		for (i = 0; i < size; i++) {
-			for (j = 0; j < size; j++) {
+	for (int k = 0; k < size; k++) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
 				if (matrix[i][k] + matrix[k][j] < matrix[i][j])
 					matrix[i][j] = matrix[i][k] + matrix[k][j];
 			}
