525af2b145884ba558d69e77a52f97e7de55276c
chewyungchung <a0133662@u.nus.edu>
2016-10-16 22:44:08 +0800

    Revised brief test to check implementation of grouping of clauses

--- UnitTesting/TestQueryValidator.cpp
+++ UnitTesting/TestQueryValidator.cpp
@@ -20,7 +20,7 @@ namespace UnitTesting {
 			// Brief testing. More comprehensive test to be created by Cher Lin
 
 			string declaration = "stmt s1,s2; assign a1,a2; while w1,w2; if if1,if2; call call1,call2; constant c1,c2; prog_line n1,n2; procedure proc1, proc2; variable v1,v2;";
-			string query_select = "Select BOOLEAN ";
+			string query_select = "Select s1 ";
 			string query_clause = "such that Follows(s1,s2) and Follows*(s1,s2) and Parent(w1,n2) and Parent*(if1,n2) such that Call(proc1,proc2) and Call*(proc1,proc2) and Next(n1,a2) and Next*(n2, w2) and Modifies(n1,\"x\") and Modifies(proc1,v1) and Modifies(\"main\",\"var2\") and Uses(if1,\"y\") and Uses(proc2, v2) and Uses(\"main\", \"var1\")";
 			string query_string = declaration + query_select + query_clause;
 			QueryValidator query_parser(query_string);
@@ -38,6 +38,11 @@ namespace UnitTesting {
 			query_tree = query_parser.Parse();
 
 			query_clause = "such that Follows(a2,5) and Follows(1,2) and Uses(5,\"y\") and Parent(if1,a1) and Parent(if2,a2) and Next(a2,n2) and Next(n2,s1) and Modifies(s1,v1) and Uses(proc1,v1) and Call(proc1,proc2) and Modifies(s1,v1) and Uses(proc1,v1) with a2.stmt#=1 and 1=0";
+			query_string = declaration + query_select + query_clause;
+			query_parser = QueryValidator(query_string);
+			query_tree = query_parser.Parse();
+
+			query_clause = "such that Follows(s1,s2) and Parent(s2, if1) such that Follows(a1,a2) and Parent(n1,n2) and Uses(a1,\"x\") and Modifies(a2,\"y\") with call1.procName=v1.varName and v1.varName=v2.varName such that Parent(w2,if2)";
 			query_string = declaration + query_select + query_clause;
 			query_parser = QueryValidator(query_string);
 			query_tree = query_parser.Parse();
